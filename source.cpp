5787c3a670b0869ea13212c86763ee7b9003d422
kellykim <hkim12595@gmail.com>
2024-02-22 21:58:40 -0800

    First Commit

---
+++ source.cpp
@@ -0,0 +1,20 @@
+// The goal of this program is to generate an efficient algorithm for the following problem:
+
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// *** Note that you must do this in-place without making a copy of the array.
+
+#include <iostream>
+using namespace std;
+
+void moveZeroes(vector<int>& nums);
+
+int main() {
+
+    // Function call should appear here
+    // Create an object in main or the function itself at your discretion
+
+
+    return 0;
+}
+
+void moveZeroes(vector<int>& nums) {}
